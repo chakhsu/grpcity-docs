7d24287d59f1a0c0479fc6b8908061a43d779461
Chakhsu.Lau <zexu.liu@live.cn>
2023-11-20 01:53:12 +0800

    chore: adjust meta

--- pages/en/_meta.ts
+++ pages/en/_meta.ts
@@ -3,14 +3,18 @@ export default {
   start: "Get Started",
   guide: "User Guide",
   apis: "API",
-  changelog: {
-    title: "Change Log ↗",
-    href: "https://github.com/chakhsu/grpcity/releases",
-    newWindow: true,
-  },
   "-- usecase": {
     type: "separator",
     title: "Use Cases",
   },
   "db-service": "DB Service",
+  "-- other": {
+    type: "separator",
+    title: "Other",
+  },
+  changelog: {
+    title: "Change Log ↗",
+    href: "https://github.com/chakhsu/grpcity/releases",
+    newWindow: true,
+  },
 }
--- pages/zh/_meta.ts
+++ pages/zh/_meta.ts
@@ -3,14 +3,18 @@ export default {
   start: "快速开始",
   guide: "用户指南",
   apis: "API",
-  changelog: {
-    title: "更新日志 ↗",
-    href: "https://github.com/chakhsu/grpcity/releases",
-    newWindow: true,
-  },
   "-- usecase": {
     type: "separator",
     title: "用例",
   },
   "db-service": "DB Service",
+  "-- other": {
+    type: "separator",
+    title: "其他",
+  },
+  changelog: {
+    title: "更新日志 ↗",
+    href: "https://github.com/chakhsu/grpcity/releases",
+    newWindow: true,
+  },
 }
