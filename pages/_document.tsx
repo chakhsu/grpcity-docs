268d9d25bba62dfa72f88b3114888c25e9196730
Chakhsu.Lau <zexu.liu@live.cn>
2024-01-12 00:23:20 +0800

    chore: add speed-insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { SkipNavLink } from 'nextra-theme-docs'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 class MyDocument extends Document {
   render() {
@@ -10,6 +11,7 @@ class MyDocument extends Document {
           <SkipNavLink styled />
           <Main />
           <NextScript />
+          <SpeedInsights />
         </body>
       </Html>
     )
